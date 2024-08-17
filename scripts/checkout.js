29ab2969dc3ede9558909725548ea84dcf34f435
Josue Suarez <jsuarez1670@gmail.com>
2024-08-17 12:27:39 -0500

    creacion de un objeto carrito

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,5 @@
 import { renderRevision } from "./checkout/orderSummary.js";
 import { renderPayment } from "./checkout/paymentSummary.js";
+import'../data/carrito-oop.js';
 renderRevision();
 renderPayment();
