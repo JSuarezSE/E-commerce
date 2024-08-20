7941b0bcf4da3cd866d79c9505048ee294096510
Josue Suarez <jsuarez1670@gmail.com>
2024-08-19 21:48:53 -0500

    promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,7 +3,18 @@ import { renderRevision } from "./checkout/orderSummary.js";
 import { renderPayment } from "./checkout/paymentSummary.js";
 //import'../data/carrito-class.js';
 //import '../data/backend-practica.js'
-loadProducts(()=>{
+
+new Promise((resolve)=>{
+  loadProducts(()=>{
+    resolve();
+  });
+}).then(()=>{
   renderRevision();
   renderPayment();
 });
+/*
+loadProducts(()=>{
+ renderRevision();
+  renderPayment();
+});
+*/
